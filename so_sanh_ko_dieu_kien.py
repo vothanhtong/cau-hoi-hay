d35605c95377f457f1cde36cc3458dbb16ab2000
vothanhtong <vothanhtong9295@gmail.com>
2024-10-15 21:26:53 +0700

    caro 9x9

--- so_sanh_ko_dieu_kien.py
+++ so_sanh_ko_dieu_kien.py
@@ -1,14 +1,14 @@
-# # Compare the largest and smallest numbers without if,else and min max and >,<,== 
-# print("so sánh số lớn nhất và nhỏ nhất:")
-# a = int(input("nhập vào số thứ nhất: "))
-# b = int(input("nhập vào số thứ hai: "))
-# # Tính toán
-# result = ( a+b + abs(b - a)) / 2
-# # In kết quả
-# print(f" Số lớn nhất là : {result}")
+# Compare the largest and smallest numbers without if,else and min max and >,<,== 
+print("so sánh số lớn nhất và nhỏ nhất:")
+a = int(input("nhập vào số thứ nhất: "))
+b = int(input("nhập vào số thứ hai: "))
+# Tính toán
+result = ( a+b + abs(b - a)) / 2
+# In kết quả
+print(f" Số lớn nhất là : {result}")
 
-# DO MAT KHAU
-## Người dùng nhập mật khẩu bí mật của họ
+# DOI MAT KHAU
+# Người dùng nhập mật khẩu bí mật của họ
 user_password = int(input("Hãy nhập mật khẩu bất kỳ: "))
 
 # Bắt đầu dò từ 0

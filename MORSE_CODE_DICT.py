1d5435def2a0c77131e58ad7d89c83fa942e1e83
Võ Thanh Tòng <vothanhtong9295@gmail.com>
2025-03-22 14:18:20 +0700

    add đèn giao thông

--- MORSE_CODE_DICT.py
+++ MORSE_CODE_DICT.py
@@ -4,11 +4,12 @@ MORSE_CODE_DICT = {
     '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z', '-----': '0', '.----': '1', '..---': '2', '...--': '3',
     '....-': '4', '.....': '5', '-....': '6', '--...': '7', '---..': '8', '----.': '9'
 }
-def morse_to_text(morse_code):
+
+def morse(morse_code):
     return ' '.join(
         ''.join(MORSE_CODE_DICT.get(letter, '') for letter in word.split())
         for word in morse_code.split('   ')
     )
-# Nhận đầu vào từ người dùng
+
 user_input = input("Nhập mã Morse: ")
-print("Kết quả: ", morse_to_text(user_input))
+print("Kết quả: ", morse(user_input))
